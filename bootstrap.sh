b74182da434ab77a2bd0f22d0ba77d62f2b0cb7e
John Norwood <norwood.john.m@gmail.com>
2023-09-19 20:15:09 +0100

    chore: slightly improves bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 set -o errexit; set -o nounset; set -o pipefail
 
+# Install the salt-minion binary if it doesn't already exist
 if ! hash salt-minion; then
 	curl -L https://bootstrap.saltstack.com | sudo sh
-	sh -c "echo 'file_client: local' >> /etc/salt/minion"
 fi
 
+# In masterless mode we don't need the salt-minion service running
 if ! systemctl is-enabled salt-minion; then
-	systemctl enable salt-minion
-	systemctl start salt-minion
+	systemctl disable salt-minion
 fi
 
+# Setup the salt-minion to run masterless
+sed -i 's|#file_client: remote|file_client: local|g' /etc/salt/minion
+
+# Symlink the state tree into place
 ln -svf $(pwd)/states /srv/salt
 ln -svf $(pwd)/pillar /srv/pillar
 ln -svf $(pwd)/files /srv/salt/files
